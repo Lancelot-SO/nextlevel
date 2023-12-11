55004d827dd6286bf6be0af0bd20d3240fe6cc12
Lancelot-SO <fsowah001@gmail.com>
2023-12-11 21:37:32 +0000

    create login and registration module

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
+import Header from "@/components/layout/Header";
 import { Roboto } from 'next/font/google'
-import './globals.css'
+import './globals.css';
+import AppProvider from "@/components/AppContext";
+import Footer from "../components/layout/Footer";
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
@@ -10,12 +13,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={roboto.className}>
-      <main className='max-w-4xl mx-auto p-4'>
-      {children}
-      </main>
-      
+        <main className="max-w-4xl mx-auto p-4">
+          <AppProvider>
+            <Header />
+            {children}
+            <Footer />
+          </AppProvider>
+        </main>
       </body>
     </html>
   )
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,17 +1,46 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 
 export default function Header() {
+    const session = useSession();
+    const status = session?.status;
+    const userData = session.data?.user;
+    let userName = userData?.name || userData?.email;
+    if (userName && userName.includes(' ')) {
+        userName = userName.split(' ')[0];
+    }
+
     return (
         <header className="flex items-center justify-between">
-            <Link className="text-primary font-semibold text-2xl" href="">Pizzariba</Link>
             <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-                <Link href={''}>Home</Link>
+                <Link className="text-primary font-semibold text-2xl" href={'/'}>Pizzariba</Link>
+                <Link href={'/'}>Home</Link>
                 <Link href={''}>Menu</Link>
                 <Link href={''}>About</Link>
                 <Link href={''}>Contact</Link>
-                <Link className="bg-primary text-white rounded-full px-8 py-2" href={''}>Login</Link>
-
+            </nav>
+            <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+                {
+                    status === 'authenticated' && (
+                        <>
+                            <Link className="whitespace-nowrap " href={'/profile'}>Hello, {userName}</Link>
+                            <button onClick={() => signOut()} className="bg-primary text-white 
+                rounded-full px-8 py-2" >Logout</button>
+                        </>
+                    )
+                }
+                {
+                    status === 'unauthenticated' && (
+                        <>
+                            <Link href={'/login'}>Login</Link>
+                            <Link className="bg-primary text-white 
+                rounded-full px-8 py-2" href={'/register'}>Register</Link>
+                        </>
+                    )
+                }
             </nav>
 
         </header>
